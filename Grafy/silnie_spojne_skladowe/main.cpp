b39b38344ebc34183b2c207a398dfc0d67f3e144
JakubZojdzik <jakub.zojdzik@gmail.com>
2021-12-15 22:29:46 +0100

    Silnie spójne składowe

--- Grafy/silnie_spojne_skladowe/main.cpp
+++ Grafy/silnie_spojne_skladowe/main.cpp
@@ -9,37 +9,28 @@ int skladowa[10007];
 
 void dfs(int a)
 {
+    seen[a] = true;
     for(int i = 0; i < kraw[a].size(); i++)
-    {
         if(!seen[kraw[a][i]])
-        {
-            seen[kraw[a][i]] = true;
             dfs(kraw[a][i]);
-        }
-    }
     S.push(a);
 }
 
 void dfs2(int a, int c)
 {
     skladowa[a] = c;
-    for(int i = 0; i < kraw[a].size(); i++)
-    {
-        if(!seen[kraw[a][i]])
-        {
-            seen[kraw[a][i]] = true;
-            dfs(kraw[a][i]);
-        }
-    }
-    S.push(a);
+    seen[a] = true;
+    for(int i = 0; i < reverted[a].size(); i++)
+        if(!seen[reverted[a][i]])
+            dfs2(reverted[a][i], c);
 }
 
 int main()
 {
-    cin.tie(NULL); cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL); cout.tie(NULL);
+    // ios_base::sync_with_stdio(false);
 
-    int n, m, a, b, c;
+    int n, m, a, b;
     cin >> n >> m; // n - wierzchołki, m - krawędzie
     for(int i = 0; i < m; i++)
     {
@@ -48,17 +39,23 @@ int main()
         reverted[b].push_back(a);
     }
     for(int i = 1; i <= n; i++) if(!seen[i]) dfs(i);
-
+    for(int i = 1; i <= n; i++) seen[i] = false;
+    int c = 0;
     while(!S.empty())
     {
         int v = S.top();
-        int c = 0;
         S.pop();
         if(!seen[v]) dfs2(v, ++c);
     }
 
-    for(int i = 1; i < n; i++)
-    {
+    for(int i = 1; i <= n; i++)
         cout << "Wierzchołek " << i << " należy do " << skladowa[i] << '\n';
-    }
 }
+
+/*
+4 4
+1 3
+3 2
+2 1
+4 3
+*/
