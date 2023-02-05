c85d05345531117154d9e35aeb2db8e14b5a37a9
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-02-05 19:54:06 +0100

    huffman

--- huffmanCoding/main.cpp
+++ huffmanCoding/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class node //węzeł z grafu
+class node
 {
 public:
     int id;
@@ -46,9 +46,9 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    float czestotliwosc[] = {8.2, 1.5, 2.8, 4.3, 13.0, 2.2, 2.0, 6.1, 7.0, 0.15, 0.77, 4.0, 2.4, 
+    float czestotliwosc[] = {8.2, 1.5, 2.8, 4.3, 13.0, 2.2, 2.0, 6.1, 7.0, 0.15, 0.77, 4.0, 2.4,
                             6.7, 7.5, 1.9, 0.095, 6.0, 6.3, 9.1, 2.8, 0.98, 2.4, 0.15, 2.0, 0.074}; //0 - a, 34 - z. Brak liter wielkich i polskich
-    string a = "Linux jest systemem wielokrotnie lepszym od systemu operacyjnego windows";
+    string a = "Ala ma kota kot ma ale atokanapapanakota";
     int dlugosc1 = a.size() * 8;
     string gotowy = tnij(a);
     licznik = gotowy.size();
@@ -81,7 +81,7 @@ int main()
                 {
                     drzewo[licznik] = drzewo[pier] + drzewo[drug];
                     drzewo[licznik].id = licznik;
-                    drzewo[pier].wyzszy = licznik; 
+                    drzewo[pier].wyzszy = licznik;
                     drzewo[drug].wyzszy = licznik;
                     licznik++;
                     break;
@@ -169,22 +169,22 @@ int printFromTree(char a)
 
 string tnij(string napis)
 {
-    int index = 0;    
+    int index = 0;
     int a = 0;
-    for(int i = 0; i < napis.size(); i++) 
-    { 
-        int j;   
-        for (j = 0; j < i; j++)  
-            if (napis[i] == napis[j]) 
+    for(int i = 0; i < napis.size(); i++)
+    {
+        int j;
+        for(j = 0; j < i; j++)
+            if (napis[i] == napis[j])
             {
                 a++;
-                break; 
+                break;
             }
         if(j == i)
-            napis[index++] = napis[i]; 
-    } 
+            napis[index++] = napis[i];
+    }
     napis.resize(napis.size()-a);
-    return napis; 
+    return napis;
 }
 
 node node::operator+(node & ref_node)
@@ -202,7 +202,3 @@ node node::operator+(node & ref_node)
     }
     return wynik;
 }
-
-//Jakub Żojdzik klasa 1A
-//cpp lepszy, w pythonie nie dalo by sie zrobic choc troche tak ladnego kodu
-//Pozdrawiam
