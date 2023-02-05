20b38efd2d34add70e8da6c3d79b4543dcf328ff
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-02-05 23:08:10 +0100

    manacher

--- manacher/main.cpp
+++ manacher/main.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int R[2][1000007];
+int R[2][500007];
 
 int main()
 {
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
     string s;
     cin >> s;
     int N = s.size();
@@ -12,18 +15,18 @@ int main()
 
     s = "@" + s + "#";
 
-    for (int t = 0; t <= 1; t++)
+    for(int t = 0; t <= 1; t++)
     {
         R[t][0] = 0;
         rp = 0;
         i = 1;
-        while (i <= N)
+        while(i <= N)
         {
-            while (s[i - rp - 1] == s[i + t + rp])
+            while(s[i - rp - 1] == s[i + t + rp])
                 rp++;
             R[t][i] = rp;
             k = 1;
-            while ((R[t][i - k] != rp - k) && (k < rp))
+            while((R[t][i - k] != rp - k) && (k < rp))
             {
                 R[t][i + k] = min(R[t][i - k], rp - k);
                 k++;
@@ -37,13 +40,13 @@ int main()
 
     for (i = 1; i <= N; i++)
     {
-        for (int j = 0; j <= 1; j++)
-            for (rp = R[j][i]; rp > 0; rp--)
+        for(int j = 0; j <= 1; j++)
+            for(rp = R[j][i]; rp > 0; rp--)
             {
-                for (k = 1; k < i - rp; k++)
+                for(k = 1; k < i - rp; k++)
                     cout << " ";
-                cout << s.substr(i - rp - 1, 2 * rp + j) << endl;
+                cout << s.substr(i - rp - 1, 2 * rp + j) << '\n';
             }
     }
-    cout << endl;
+    cout << '\n';
 }
