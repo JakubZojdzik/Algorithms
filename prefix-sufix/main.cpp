1a0c79e279804737c0dfd895874c4f431fe716fb
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-02-06 00:03:37 +0100

    prefix sufix - preprocesing for KMP

---
+++ prefix-sufix/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ps[100007];
+
+int main()
+{
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    string s;
+    s = "aababaa";
+    s = "!" + s;
+    ps[0] = -1;
+    int b = -1;
+    for(int i = 1; i < int(s.size()); i++)
+    {
+        while(b > -1 && s[i] != s[b+1])
+        {
+            b = ps[b];
+        }
+        b++;
+        ps[i] = b;
+    }
+}
+
+/*
+aabaababaa
+*/
