9a58e3624449b957b92bba77a991b0232f35a68c
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-02-06 20:33:31 +0100

    Floyd Warshall algorithm

--- floydWarshall/main.cpp
+++ floydWarshall/main.cpp
@@ -7,7 +7,6 @@ ll dp[107][107];
 
 int main()
 {
-    // O(n^3)
     cin.tie(0); cout.tie(0);
     ios_base::sync_with_stdio(0);
     int n, m, a, b, c;
@@ -20,7 +19,7 @@ int main()
             if(i == j) dp[i][j] = 0;
         }
     }
-    for (int i = 0; i < m; i++)
+    for(int i = 0; i < m; i++)
     {
         cin >> a >> b >> c;
         kraw[a].push_back(make_pair(b, c));
@@ -32,40 +31,17 @@ int main()
             for(int j = 1; j <= n; j++)
                 if(dp[i][k] != LONG_LONG_MAX && dp[k][j] != LONG_LONG_MAX && dp[i][j] > dp[i][k] + dp[k][j])
                     dp[i][j] = dp[i][k] + dp[k][j];
-    
+
     for(int i = 1; i <= n; i++)
     {
         if(dp[i][i] < 0)
         {
-            cout << "GRAF ZAWIERA UJEMNY CYKL!!!\n";
+            cout << "Negative cicle\n";
+            return 0;
         }
     }
-    
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= n; j++)
-        {
-            cout << "dp[" << i << "][" << j << "] = ";
-            if(dp[i][j] == LONG_LONG_MAX) cout << "NO PATH";
-            else cout << dp[i][j];
-            cout << '\n';
-        }
-    }
-}
 
-/*
-5 13
-1 2 5 
-1 3 4 
-1 4 8
-2 1 -4 
-2 3 -2
-2 5 5
-3 4 5 
-3 5 2
-4 1 -1 
-4 2 2 
-4 5 -1
-5 3 4 
-5 4 2
-*/
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            cout << "dp[" << i << "][" << j << "] = " << dp[i][j] << '\n';
+}
